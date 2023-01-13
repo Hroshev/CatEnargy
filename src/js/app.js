7cefb92bddf148c2b3257ca32e30ffd959bd4a30
Ivan Hroshev <grosheff.ivan@gmail.com>
2023-01-13 12:39:24 +0200

    first commit

--- src/js/app.js
+++
@@ -1,4 +0,0 @@
-import * as burger from "./modules/burger.js"
-import * as slider from "./modules/slider.js"
-import * as map from "./modules/map.js"
-import * as product from "./modules/products.js"
